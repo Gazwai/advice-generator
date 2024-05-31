909e2b534913d71cd4cc4007fc8e0b926ff114ea
Gary Y <garyyau080@gmail.com>
2024-06-01 00:53:13 +0900

    refactor: remove useEffect update metadata

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import Image from 'next/image';
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 import useGetRequest from './hooks/useFetch';
 
 export default function Home() {
@@ -19,17 +19,20 @@ export default function Home() {
     mutate();
   };
 
+  const adviceId = data?.slip?.id || '0';
+  const advice = data?.slip?.advice || 'Loading advice...';
+
   return (
     <main className="flex flex-col items-center justify-between min-h-screen p-24">
       <div className="flex flex-col bg-slate-700 relative w-96 min-h-80 rounded-xl px-8 pt-12 pb-20 md:w-[32rem]">
         <div className="flex flex-col items-center gap-4">
           <p className="text-center text-green-300 font-semibold tracking-[.5em]">
-            ADVICE #{data?.slip.id ? data?.slip.id : '0'}
+            ADVICE #{adviceId}
           </p>
 
           <p className="min-h-40 flex flex-col justify-center w-4/5 mb-8 text-2xl font-medium text-center">
             &quot;
-            {data?.slip.advice ? data?.slip.advice : 'Loading advice...'}
+            {advice}
             &quot;
           </p>
         </div>
@@ -43,19 +46,16 @@ export default function Home() {
         />
 
         <button
+          aria-label="Load new advice"
           disabled={loading}
           onClick={handleMutate}
           className="absolute top-[90%] left-[39%] md:left-[42%] flex items-center justify-center
             h-20 w-20 rounded-full bg-green-300 hover:shadow-[0_0_3rem_0.5rem_rgba(0,0,0,0.3)]
           hover:shadow-green-300 disabled:cursor-wait"
         >
-          <Image
-            className={loading ? 'animate-spin' : ''}
-            src="/icon-dice.svg"
-            width={36}
-            height={36}
-            alt="dice"
-          />
+          <div className={loading ? 'animate-spin' : ''}>
+            <Image src="/icon-dice.svg" width={36} height={36} alt="dice" />
+          </div>
         </button>
       </div>
     </main>
