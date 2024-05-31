f4cb254dfc6fdfaaf36d5b71fbf59a3a383b9d5c
Gary Y <garyyau080@gmail.com>
2024-06-01 00:39:33 +0900

    reformat: Add a spinner to the dice

--- app/page.tsx
+++ app/page.tsx
@@ -5,23 +5,16 @@ import { useEffect, useState } from 'react';
 import useGetRequest from './hooks/useFetch';
 
 export default function Home() {
-  const [slip, setSlip] = useState({ id: 0, advice: 'Loading advice...' });
   const [loading, setLoading] = useState(false);
 
   const { data, mutate } = useGetRequest('https://api.adviceslip.com/advice');
-
-  useEffect(() => {
-    if (data?.slip) {
-      setSlip(data.slip);
-    }
-  }, [data?.slip]);
 
   const handleMutate = async () => {
     setLoading(true);
 
     setTimeout(() => {
       setLoading(false);
-    }, 2000);
+    }, 1000);
 
     mutate();
   };
@@ -31,12 +24,12 @@ export default function Home() {
       <div className="flex flex-col bg-slate-700 relative w-96 min-h-80 rounded-xl px-8 pt-12 pb-20 md:w-[32rem]">
         <div className="flex flex-col items-center gap-4">
           <p className="text-center text-green-300 font-semibold tracking-[.5em]">
-            ADVICE #{slip.id ? slip.id : '0'}
+            ADVICE #{data?.slip.id ? data?.slip.id : '0'}
           </p>
 
           <p className="min-h-40 flex flex-col justify-center w-4/5 mb-8 text-2xl font-medium text-center">
             &quot;
-            {slip.advice ? slip.advice : 'Loading advice...'}
+            {data?.slip.advice ? data?.slip.advice : 'Loading advice...'}
             &quot;
           </p>
         </div>
@@ -56,7 +49,13 @@ export default function Home() {
             h-20 w-20 rounded-full bg-green-300 hover:shadow-[0_0_3rem_0.5rem_rgba(0,0,0,0.3)]
           hover:shadow-green-300 disabled:cursor-wait"
         >
-          <Image src="/icon-dice.svg" width={36} height={36} alt="dice" />
+          <Image
+            className={loading ? 'animate-spin' : ''}
+            src="/icon-dice.svg"
+            width={36}
+            height={36}
+            alt="dice"
+          />
         </button>
       </div>
     </main>
