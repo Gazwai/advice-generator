2f5269a092ee38101ce7a42ef10ba19f9a37f6c9
Gary Y <garyyau080@gmail.com>
2024-05-31 23:16:58 +0900

    style: Fix some tailwind and responsiveness

--- app/page.tsx
+++ app/page.tsx
@@ -4,38 +4,37 @@ import Image from 'next/image';
 import { useEffect, useState } from 'react';
 
 export default function Home() {
-  const [advice, setAdvice] = useState('Loading advice...');
+  const [data, setData] = useState({ id: 0, advice: 'Loading advice...' });
 
-  // Function to fetch advice from the API
   const fetchAdvice = async () => {
     try {
       const res = await fetch('https://api.adviceslip.com/advice');
       const data = await res.json();
-      console.log('🚀 ~ fetchAdvice ~ data:', data);
-      setAdvice(data.slip.advice);
+      setData(data.slip);
     } catch (err) {
       console.error(err);
-      setAdvice('Failed to fetch advice.');
     }
   };
 
-  // useEffect to call fetchAdvice on component mount
   useEffect(() => {
     fetchAdvice();
   }, []);
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="flex flex-col bg-slate-800 relative min-w-96 min-h-96 rounded-xl px-8 pt-12 pb-20">
+      <div className="flex flex-col bg-slate-700 relative w-96 min-h-80 rounded-xl px-8 pt-12 pb-20 md:w-[32rem]">
         <div className="flex flex-col items-center gap-4">
-          <p className="text-center text-green-800 font-semibold">Advice #1</p>
+          <p className="text-center text-green-300 font-semibold tracking-[.5em]">
+            ADVICE #{data?.id}
+          </p>
 
-          <p className="flex flex-col justify-center text-center min-h-64 w-4/5 p-auto">
-            {advice}
+          <p className="flex flex-col justify-center text-center min-h-40 w-4/5 p-auto text-2xl font-medium">
+            &quot;{data?.advice}&quot;
           </p>
         </div>
 
         <Image
+          className="md:w-[32rem]"
           src="/pattern-divider-mobile.svg"
           width={320}
           height={18}
@@ -44,7 +43,7 @@ export default function Home() {
 
         <button
           onClick={fetchAdvice}
-          className="absolute top-[90%] left-[39%] flex items-center justify-center h-20 w-20 rounded-full bg-green-300"
+          className="absolute top-[90%] left-[39%] md:left-[42%] flex items-center justify-center h-20 w-20 rounded-full bg-green-300 hover:shadow-[0_0_3rem_0.5rem_rgba(0,0,0,0.3)] hover:shadow-green-300 "
         >
           <Image src="/icon-dice.svg" width={36} height={36} alt="dice" />
         </button>
